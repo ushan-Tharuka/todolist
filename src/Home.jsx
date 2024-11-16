f7ad4ee1bfd5ea84aafbf4da717c109311f75741
ushan-Tharuka <ushantharuka2222@gmail.com>
2024-11-16 07:19:36 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import Create from './Create'
+
+function Home() {
+    const [todos, setTodos] = useState([])
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <Create/>
+      {
+        todos.length === 0 ?
+        <div><h2>No record</h2></div>
+        :
+        todos.map(todo =>(
+            <div>
+                {todo}
+            </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default Home
