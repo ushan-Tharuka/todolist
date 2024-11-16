7e2d0577f7a568668860a2dac2fc6afd02076a7a
ushan-Tharuka <ushantharuka2222@gmail.com>
2024-11-16 07:52:55 +0530

    update UI

--- src/Create.jsx
+++ src/Create.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Create() {
   return (
-    <div>
+    <div className="create_form">
         <input type="text" name="" id="" />
         <button type="button">Add</button>
     </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -4,11 +4,12 @@ import Create from './Create'
 function Home() {
     const [todos, setTodos] = useState([])
   return (
-    <div>
+    <div className='home'>
       <h2>Todo List</h2>
-      <Create/>
+      <Create />
       {
-        todos.length === 0 ?
+        todos.length === 0 
+        ?
         <div><h2>No record</h2></div>
         :
         todos.map(todo =>(
