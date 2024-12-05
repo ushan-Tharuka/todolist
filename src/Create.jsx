32893cfadc683da20f76b7ce7983703f58d9a35c
ushan-Tharuka <ushantharuka2222@gmail.com>
2024-12-05 09:54:52 +0530

    succesfully update mongodb database

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Create() {
+  const [task, setTask] = useState('');
+
+  const handleAdd = () => {
+    console.log('Sending task:', task);
+    axios
+      .post('http://localhost:3001/add', { task }, {
+        headers: { 'Content-Type': 'application/json' }, // Set the content type explicitly
+      })
+      .then((result) => console.log(result.data))
+      .catch((err) => console.log(err));
+  };
+  
   return (
     <div className="create_form">
-        <input type="text" name="" id="" />
-        <button type="button">Add</button>
+        <input type="text" placeholder='Enter Task' onChange={(e) => setTask(e.target.value)} />
+        <button type="button" onClick={handleAdd}>Add</button>
     </div>
-  )
+  );
 }
 
-export default Create
+export default Create;
+
