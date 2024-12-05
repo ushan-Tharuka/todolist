6adea950871a66efa330ef3adc6078b29fb47146
ushan-Tharuka <ushantharuka2222@gmail.com>
2024-12-05 10:04:42 +0530

    get data from mongo db

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Create from './Create'
+import axios from 'axios'
 
 function Home() {
     const [todos, setTodos] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:3001/get')
+        .then(result => setTodos(result.data))
+        .catch(err => console.log(err))
+    }, [])
   return (
     <div className='home'>
       <h2>Todo List</h2>
@@ -14,7 +20,7 @@ function Home() {
         :
         todos.map(todo =>(
             <div>
-                {todo}
+                {todo.task}
             </div>
         ))
       }
